df71c948e2f1189bc04456b6e4b0f3376c8e1fa7
Kristina Brueva <kristinabruuu@gmail.com>
2023-05-04 15:00:13 +1000

    Fix styles for mobile

--- src/components/cats.tsx
+++ src/components/cats.tsx
@@ -21,12 +21,12 @@ const Cats: React.FunctionComponent = () => {
         <div className="bg-lavender absolute top-24 w-full h-[95%] -z-50" />
         <ul
           role="list"
-          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-y-8 gap-x-8 text-center lg:mx-0 lg:max-w-none lg:grid-cols-5"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 sm:grid-cols-3 gap-y-8 gap-x-8 text-center lg:mx-0 lg:max-w-none lg:grid-cols-5"
         >
           {cats.map((name, index) => {
             return (
               <li key={index}>
-                <div className="relative mx-auto h-48 w-48">
+                <div className="relative mx-auto h-40 w-40 sm:h-48 sm:w-48">
                   <Image
                     fill
                     alt="picture of a cat"
