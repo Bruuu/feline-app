d8742993ef67e96aa5b68ce97dc062ef8daf76ef
Kristina Brueva <kristinabruuu@gmail.com>
2023-05-05 10:48:08 +1000

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="large icon" sizes="96x96" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
